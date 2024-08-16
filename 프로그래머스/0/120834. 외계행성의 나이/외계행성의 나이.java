a41d3202a9a3cdd8fd3674cbe6e3d69509cd1348
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2024-08-16 16:00:14 +0900

    [level 0] Title: 외계행성의 나이, Time: 13.42 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        for(char c : String.valueOf(age).toCharArray())
+            answer += (char) (c-'0'+'a');
+        return answer;
+    }
+}

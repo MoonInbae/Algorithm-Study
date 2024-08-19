885d794b6c844cd0b32bf5d61ccca34a0676359f
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2024-08-19 15:47:21 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 3.50 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -3,9 +3,8 @@ import java.util.*;
 class Solution {
     public int[] solution(String my_string) {
         List<Integer> list = new ArrayList<>();
-        for (char c : my_string.toCharArray()) {
+        for (char c : my_string.toCharArray())
             if(c >= '0' && c <= '9') list.add((int) c-'0');
-        }
         list.sort(Comparator.naturalOrder());
         return list.stream().mapToInt(Integer::intValue).toArray();
     }

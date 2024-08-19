2ee72041bfaec3c2c74a901f1e99dd77e54a4554
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2024-08-19 15:02:39 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,9 +1,8 @@
 class Solution {
     public int solution(int n) {
         int i = 1;
-        for(i=1; i<=10; i++) {
+        for(i=1; i<=10; i++)
             if((n /= i) < i+1) return i;
-        }
         return i-1;
     }
 }

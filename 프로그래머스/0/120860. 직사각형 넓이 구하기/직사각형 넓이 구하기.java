a498e61f0b7d5931f5c357745ce66459d0c6997f
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2024-08-19 16:33:59 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.13 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -4,14 +4,12 @@ class Solution {
     public int solution(int[][] dots) {
         List<Integer> xList = new ArrayList<>();
         List<Integer> yList = new ArrayList<>();
-        int width = 0;
-        int length = 0;
         for(int[] dot : dots) {
             xList.add(dot[0]);
             yList.add(dot[1]);
         }
-        width = Collections.max(xList) - Collections.min(xList);
-        length = Collections.max(yList) - Collections.min(yList);
+        int width = Collections.max(xList) - Collections.min(xList);
+        int length = Collections.max(yList) - Collections.min(yList);
         return width * length;
     }
 }

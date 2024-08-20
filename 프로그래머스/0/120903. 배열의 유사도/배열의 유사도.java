c64c8dec59667b585607849e7badbf0bb9038109
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2024-08-20 18:01:55 +0900

    [level 0] Title: 배열의 유사도, Time: 0.12 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        List<String> list = new ArrayList<>(Arrays.asList(s1));
+        int cnt = 0;
+        for(String s : s2) if(list.contains(s)) cnt++;
+        return cnt;
+    }
+}

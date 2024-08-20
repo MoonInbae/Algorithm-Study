68cbc739c9c9ae496eb7e1a57beb08dd511d02b6
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2024-08-20 21:58:17 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int cnt = 0;
+        for(int n : array)
+            if(height < n) cnt++;
+        return cnt;
+    }
+}

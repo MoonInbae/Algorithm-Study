2ce2b09f94243cb768ab98043056297ab934ef91
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2024-09-11 13:58:59 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] lines) {
+        int[] entire = new int[200];
+        int answer = 0;
+        for(int[] line : lines) {
+            for(int i=line[0]+100; i<line[1]+100; i++) {
+                entire[i]++;
+            }
+        }
+        for(int i=0; i<entire.length; i++) {
+            if(entire[i] > 1) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}

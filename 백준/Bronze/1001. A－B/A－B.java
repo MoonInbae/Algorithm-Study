496b8ec11c76571b8acd8d8ace4b0071127faf03
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2025-01-15 03:22:49 +0900

    [Bronze V] Title: A-B, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -3,9 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] s = sc.nextLine().split(" ");
-        int answer = Integer.parseInt(s[0])-Integer.parseInt(s[1]);
-        System.out.println(answer);
+        System.out.println(sc.nextInt()-sc.nextInt());
         sc.close();
     }
 }

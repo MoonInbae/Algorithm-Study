661be648c29db6af7da5513f4ccc89f9f3fe587c
Inbae-Moon <81071736+MoonInbae@users.noreply.github.com>
2025-01-16 05:19:33 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 180 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+        if(a==60 && b==60 && c==60) {
+            System.out.println("Equilateral");
+            return;
+        }
+        int sum = a+b+c;
+        if(sum!=180) {
+            System.out.println("Error");
+            return;
+        }
+        if(a==b || a==c || b==c) System.out.println("Isosceles");
+        else System.out.println("Scalene");
+    }
+}
